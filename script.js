97a62192249edc76cd7bb7e5047413fbc40f4171
Marc Diallo <mark.devguru@outlook.com>
2025-08-19 17:38:47 -0500

    adds the .icon-up class to the CSS

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ const listText = document.querySelectorAll("list-element__text");
 icons.forEach(function (icon) {
   icon.addEventListener("click", function (el) {
     if (!el.target) return;
-    console.log(el);
+
   });
 });
