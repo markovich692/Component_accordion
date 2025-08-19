c7d2435fc3d8ef9eb8530db63b0f94493c6821eb
Marc Diallo <mark.devguru@outlook.com>
2025-08-19 17:32:56 -0500

    loops over the icons Nodelist usin the forEach method  and listens for the click event on each icon

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const icons = document.querySelectorAll(".icon-down");
 const listText = document.querySelectorAll("list-element__text");
+
+icons.forEach(function (icon) {
+  icon.addEventListener("click", function (el) {
+    if (!el.target) return;
+    console.log(el);
+  });
+});
