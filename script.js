747800f997d9c4cdd53fea599ad0b2d014ce062e
Marc Diallo <mark.devguru@outlook.com>
2025-08-19 18:26:13 -0500

    adds the toggle functionality to rotate the icons on click

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const icons = document.querySelectorAll(".icon-down");
 const listText = document.querySelectorAll("list-element__text");
 
 icons.forEach(function (icon) {
-  icon.addEventListener("click", function (el) {
-    if (!el.target) return;
-
+  icon.addEventListener("click", function (event) {
+    if (!event.target) return;
+    icon.classList.toggle("icon-up");
+    icon.classList.toggle("icon-down");
   });
 });
