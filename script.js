324a3cc5e973fe6c255ce4b52b10422675333765
Marc Diallo <mark.devguru@outlook.com>
2025-08-24 18:46:41 -0500

    Define the iconID/textId constant variables

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const listText = document.querySelectorAll("list-element__text");
 icons.forEach(function (icon) {
   icon.addEventListener("click", function (event) {
     if (!event.target) return;
+    const iconID = event.target.dataset.iconId;
+    const textID = event.target.dataset.textId;
+
+    console.log(iconID);
+
+    // console.log(iconID);
+
     icon.classList.toggle("icon-up");
     icon.classList.toggle("icon-down");
   });
