06f719171f840f976dc8aa758c8586e6a77f12f0
Marc Diallo <mark.devguru@outlook.com>
2025-08-24 18:53:36 -0500

    Select all of the list-elements__text correctly

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const icons = document.querySelectorAll(".icon-down");
-const listText = document.querySelectorAll("list-element__text");
+const listText = document.querySelectorAll(".list-element__text");
 
 icons.forEach(function (icon) {
   icon.addEventListener("click", function (event) {
     if (!event.target) return;
     const iconID = event.target.dataset.iconId;
     const textID = event.target.dataset.textId;
-
-    console.log(iconID);
-
-    // console.log(iconID);
 
     icon.classList.toggle("icon-up");
     icon.classList.toggle("icon-down");
